a4a23d40ed6841687801e485ee0536993fb1b240
jahidqq <jahid@quadque.digital>
2022-11-23 15:42:12 +0600

    change blog list section

--- resources/views/user/pages/blog.blade.php
+++ resources/views/user/pages/blog.blade.php
@@ -1,7 +1,6 @@
 @extends('user.layout.app')
 @section('content')
   <div class="page-wrapper">
-
       <!-- Page Banner Start -->
       @include('user.partials.banner')
       <!-- Page Banner End -->
@@ -117,130 +116,6 @@
           </div>
       </section>
       <!-- Blog Standard End -->
-
-
-      <!-- Footer Area Start -->
-      <footer class="main-footer bg-blue">
-          <div class="container">
-              <div class="footer-newsletter br-10 bg-lighter">
-                  <div class="row">
-                      <div class="col-lg-6">
-                          <div class="newsletter-video bgs-cover overlay wow fadeInLeft delay-0-2s" style="background-image: url(assets/images/video/footer-video-bg.jpg);">
-                              <a href="https://www.youtube.com/watch?v=9Y7ma241N8k" class="mfp-iframe video-play"><i class="fas fa-play"></i></a>
-                          </div>
-                      </div>
-                      <div class="col-lg-6 align-self-center">
-                          <div class="newsletter-content wow fadeInRight delay-0-2s">
-                              <div class="section-title mb-20">
-                                  <span class="sub-title-two">Newsletters</span>
-                                  <h2>Get Our Every Single Notifications</h2>
-                              </div>
-                              <form class="newsletter-form mt-25" action="#">
-                                  <div class="newsletter-radios mb-25">
-                                      <div class="custom-control custom-radio">
-                                          <input type="radio" class="custom-control-input" id="hero-wekly" name="example1" checked="">
-                                          <label class="custom-control-label" for="hero-wekly">Regular Updates</label>
-                                      </div>
-                                      <div class="custom-control custom-radio">
-                                          <input type="radio" class="custom-control-input" id="hero-monthly" name="example1">
-                                          <label class="custom-control-label" for="hero-monthly">Weekly Updates</label>
-                                      </div>
-                                  </div>
-                                  <div class="newsletter-email">
-                                      <label for="email"><i class="far fa-envelope"></i></label>
-                                      <input id="email" type="email" placeholder="Enter Email" required="">
-                                      <button type="submit" class="theme-btn style-two">sign up <i class="fas fa-arrow-right"></i></button>
-                                  </div>
-                              </form>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-              <div class="row justify-content-between text-white pt-75">
-                  <div class="col-lg-3 col-sm-4">
-                      <div class="footer-widget about-widget">
-                          <h5 class="footer-title">About Us</h5>
-                          <p>Sit amet consectetur adipiscin seeiusmod tempor incididunt ut dolore magna aliqu asusp disse ultrices gravida commodo</p>
-                          <h5 class="pt-5">Follow Us</h5>
-                          <div class="social-style-one">
-                              <a href="contact.html"><i class="fab fa-facebook-f"></i></a>
-                              <a href="contact.html"><i class="fab fa-twitter"></i></a>
-                              <a href="contact.html"><i class="fab fa-linkedin-in"></i></a>
-                              <a href="contact.html"><i class="fab fa-youtube"></i></a>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="col-lg-2 col-md-3 col-sm-4">
-                      <div class="footer-widget menu-widget">
-                          <h5 class="footer-title">Courses</h5>
-                          <ul>
-                              <li><a href="course-details.html">Life Coach</a></li>
-                              <li><a href="course-details.html">Business Coach</a></li>
-                              <li><a href="course-details.html">Health Coach</a></li>
-                              <li><a href="course-details.html">Development</a></li>
-                              <li><a href="course-details.html">Web Design</a></li>
-                              <li><a href="course-details.html">SEO Optimize</a></li>
-                          </ul>
-                      </div>
-                  </div>
-                  <div class="col-lg-2 col-md-3 col-sm-4">
-                      <div class="footer-widget menu-widget">
-                          <h5 class="footer-title">Resources</h5>
-                          <ul>
-                              <li><a href="contact.html">Community</a></li>
-                              <li><a href="contact.html">Support</a></li>
-                              <li><a href="contact.html">Video Guides</a></li>
-                              <li><a href="contact.html">Documentation</a></li>
-                              <li><a href="contact.html">Security</a></li>
-                              <li><a href="contact.html">Template</a></li>
-                          </ul>
-                      </div>
-                  </div>
-                  <div class="col-lg-5">
-                      <div class="row">
-                          <div class="col-sm-6">
-                              <div class="footer-widget contact-info-widget">
-                                  <h5 class="footer-title">Get In Touch</h5>
-                                  <ul>
-                                      <li><i class="fas fa-map-marker-alt"></i> 55 Main Street, 2nd Block, New York</li>
-                                      <li><i class="far fa-envelope"></i> <a href="mailto:support@gmail.com">support@gmail.com</a></li>
-                                      <li><i class="fas fa-phone"></i> <a href="callto:+0123456789">+012 (345) 67 89</a></li>
-                                      <li><i class="far fa-clock"></i> Sunday - Friday,<br> 08 am - 05 pm</li>
-                                  </ul>
-                              </div>
-                          </div>
-                          <div class="col-sm-6">
-                              <div class="footer-widget video-widget">
-                                  <p>Quis autem vel eum iure repre enderit voluptate</p>
-                                  <div class="video-widget overlay my-20">
-                                      <img src="assets/images/footer/video.jpg" alt="Video">
-                                      <a href="https://www.youtube.com/watch?v=9Y7ma241N8k" class="mfp-iframe video-play"><i class="fas fa-play"></i></a>
-                                  </div>
-                                  <a href="about.html" class="read-more">view more <i class="fas fa-arrow-right"></i></a>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-          <div class="copyright-area bg-dark-blue text-white rel">
-              <div class="container">
-                  <div class="copyright-inner">
-                      <p>© 2022. <a href="index.html">Wellern</a> All rights reserved.</p>
-                      <ul class="footer-menu">
-                          <li><a href="faqs.html">Faqs</a></li>
-                          <li><a href="contact.html">Links</a></li>
-                          <li><a href="about.html">About</a></li>
-                          <li><a href="contact.html">Payments</a></li>
-                      </ul>
-                  </div>
-              </div>
-              <!-- Scroll Top Button -->
-              <button class="scroll-top scroll-to-target" data-target="html"><span class="fas fa-angle-double-up"></span></button>
-          </div>
-      </footer>
-      <!-- Footer Area End -->
-
   </div>
 
 @endsection
