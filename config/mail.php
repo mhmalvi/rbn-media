c618dc860dbb51d4c8e2d37d5ffea61f138a7cd8
Habib <habib@quadque.digital>
2022-12-14 16:13:43 +0600

    student_registration

--- config/mail.php
+++ config/mail.php
@@ -32,12 +32,12 @@ return [
     |            "postmark", "log", "array", "failover"
     |
     */
-
+    // smtp . mailgun . org
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
+            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
+            'port' => env('MAIL_PORT', 465),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
@@ -95,6 +95,14 @@ return [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
+
+    // 'stream' => [
+    //     'ssl' => [
+    //         'allow_self_signed' => true,
+    //         'verify_peer' => false,
+    //         'verify_peer_name' => false,
+    //     ],
+    // ],
 
     /*
     |--------------------------------------------------------------------------
